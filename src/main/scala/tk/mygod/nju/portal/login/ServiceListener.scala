9e2eba8476b603051356f72ca8ede0df98e889fd
Mygod <mygodstudio@gmail.com>
2015-12-02 00:48:44 +0800

    Update mygod-lib-android

--- src/main/scala/tk/mygod/nju/portal/login/ServiceListener.scala
+++ src/main/scala/tk/mygod/nju/portal/login/ServiceListener.scala
@@ -3,12 +3,13 @@ package tk.mygod.nju.portal.login
 import android.content.{BroadcastReceiver, Context, Intent}
 import android.net.{ConnectivityManager, NetworkInfo}
 import android.util.Log
+import tk.mygod.content.ContextPlus
 
 object ServiceListener {
   private val TAG = "ServiceListener"
 }
 
-final class ServiceListener extends BroadcastReceiver {
+final class ServiceListener extends BroadcastReceiver with ContextPlus {
   import ServiceListener._
 
   override def onReceive(context: Context, intent: Intent) {
@@ -20,7 +21,7 @@ final class ServiceListener extends BroadcastReceiver {
         if (PortalManager.cares(n.getType))
           if (n.isConnected) PortalManager.listenerLegacy.onAvailable(n) else PortalManager.listenerLegacy.onLost(n)
       case Intent.ACTION_BOOT_COMPLETED =>
-        if (App.instance.autoConnectEnabled) context.startService(new Intent(context, classOf[PortalManager]))
+        if (App.instance.autoConnectEnabled) context.startService(intentService[PortalManager])
     }
   }
 }
