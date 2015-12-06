abfbcb711189966bdcfc84642790528831269069
Mygod <mygodstudio@gmail.com>
2015-12-07 01:43:03 +0800

    Add warning for portal webpage

--- src/main/scala/tk/mygod/nju/portal/login/SettingsFragment.scala
+++ src/main/scala/tk/mygod/nju/portal/login/SettingsFragment.scala
@@ -22,13 +22,14 @@ final class SettingsFragment extends PreferenceFragmentPlus {
   import SettingsFragment._
 
   private lazy val activity = getActivity.asInstanceOf[MainActivity]
+  private lazy val portalWeb = findPreference("auth.portalWeb")
   private lazy val useBoundConnections = findPreference("misc.useBoundConnections")
 
   def onCreatePreferences(savedInstanceState: Bundle, rootKey: String) {
     getPreferenceManager.setSharedPreferencesName(PREF_NAME)
     addPreferencesFromResource(R.xml.settings)
 
-    findPreference("auth.portalWeb").setOnPreferenceClickListener(_ => {
+    portalWeb.setOnPreferenceClickListener(_ => {
       startActivity(activity.intent[PortalActivity])
       true
     })
@@ -54,6 +55,7 @@ final class SettingsFragment extends PreferenceFragmentPlus {
     findPreference("status.area_name").setOnPreferenceClickListener(humorous)
     findPreference("status.balance").setOnPreferenceClickListener(humorous)
 
+    useBoundConnections.setOnPreferenceClickListener(_ => activity.testBoundConnections(true))
     findPreference("misc.update").setOnPreferenceClickListener(_ => {
       UpdateManager.check(activity, "https://github.com/Mygod/nju-portal-login-android/releases", app.handler)
       true
@@ -70,17 +72,18 @@ final class SettingsFragment extends PreferenceFragmentPlus {
 
   override def onResume {
     super.onResume
-
     val available = app.boundConnectionsAvailable
     useBoundConnections.setEnabled(available == 1 || available == 2)
-    useBoundConnections.setOnPreferenceClickListener(_ => activity.testBoundConnections(true))
     useBoundConnections.setSummary(getString(R.string.bound_connections_summary) + getString(available match {
       case 0 => R.string.bound_connections_summary_na
       case 1 => R.string.bound_connections_summary_permission_missing
       case 2 => R.string.bound_connections_summary_enabled_revokable
       case 3 => R.string.bound_connections_summary_enabled
     }))
-    if (available > 1 && NetworkMonitor.instance != null) NetworkMonitor.instance.initBoundConnections
+    if (available > 1) {
+      portalWeb.setSummary(R.string.settings_auth_portal_web_summary)
+      if (NetworkMonitor.instance != null) NetworkMonitor.instance.initBoundConnections
+    } else portalWeb.setSummary(null)
   }
 
   override def onDisplayPreferenceDialog(preference: Preference) = preference match {
