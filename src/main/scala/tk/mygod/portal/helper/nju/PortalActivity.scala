3400ba65d3b7839629ae782bac1464dbfdbc794a
Mygod <mygodstudio@gmail.com>
2015-12-09 21:25:52 +0800

    Export PortalActivity just for fun

--- src/main/scala/tk/mygod/portal/helper/nju/PortalActivity.scala
+++ src/main/scala/tk/mygod/portal/helper/nju/PortalActivity.scala
@@ -37,7 +37,7 @@ class PortalActivity extends ToolbarActivity with TypedFindView with OnRefreshLi
     manager.setAcceptCookie(true)
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_webview)
-    configureToolbar(0)
+    configureToolbar(R.drawable.ic_close)
     toolbar.setTitle(PortalManager.DOMAIN)
     toolbar.inflateMenu(R.menu.activity_webview)
     toolbar.setOnMenuItemClickListener(this)
