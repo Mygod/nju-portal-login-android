a0673aa9a850d50420d66eb71b2f6313a191d0de
Mygod <contact-git@mygod.be>
2016-10-13 10:59:56 +0800

    Allow system to reuse connections

--- src/main/scala/tk/mygod/portal/helper/nju/PortalManager.scala
+++ src/main/scala/tk/mygod/portal/helper/nju/PortalManager.scala
@@ -76,7 +76,7 @@ object PortalManager {
   private implicit val formats = Serialization.formats(NoTypeHints)
   private def parseResult(conn: HttpURLConnection, login: Boolean = false) = {
     //noinspection JavaAccessorMethodCalledAsEmptyParen
-    val resultStr = IOUtils.readAllText(conn.getInputStream())
+    val resultStr = autoClose(conn.getInputStream())(IOUtils.readAllText)
     if (BuildConfig.DEBUG) Log.v(TAG, resultStr)
     val json = try parse(resultStr) catch {
       case e: ParseException => throw InvalidResponseException(resultStr)
@@ -112,12 +112,13 @@ object PortalManager {
     *
     * @return 0 for success, 1 for failure, 2 for login required
     */
-  def testConnectionCore(conn: URL => URLConnection): Int =
-    try autoDisconnect(conn(new URL(HTTP, DOMAIN, "/portal_io/getinfo")).asInstanceOf[HttpURLConnection]) { conn =>
+  def testConnectionCore(connector: URL => URLConnection): Int =
+    try {
+      val conn = connector(new URL(HTTP, DOMAIN, "/portal_io/getinfo")).asInstanceOf[HttpURLConnection]
       setup(conn)
       val (code, json) = parseResult(conn)
-      if (code == 0) updateUserInfo((json \ "userinfo").asInstanceOf[JObject])  // 操作成功
-      code  // 2: 无用户portal信息
+      if (code == 0) updateUserInfo((json \ "userinfo").asInstanceOf[JObject]) // 操作成功
+      code // 2: 无用户portal信息
     } catch {
       case e: InvalidResponseException =>
         if (BuildConfig.DEBUG) Log.w(TAG, e.getMessage)
@@ -214,9 +215,10 @@ object PortalManager {
       new NotificationCompat.BigTextStyle(builder.setContentText(summary)).bigText(summary).build
     }
   }
-  private def queryOnlineCore(conn: URL => URLConnection): List[OnlineEntry] =
-    try autoDisconnect(conn(new URL(HTTP, DOMAIN, "/portal_io/selfservice/bfonline/getlist"))
-      .asInstanceOf[HttpURLConnection]) { conn =>
+  private def queryOnlineCore(connector: URL => URLConnection): List[OnlineEntry] =
+    try {
+      val conn = connector(new URL(HTTP, DOMAIN, "/portal_io/selfservice/bfonline/getlist"))
+        .asInstanceOf[HttpURLConnection]
       setup(conn, AUTH_BASE.format(username, password))
       // TODO: Support CHAP encryption check
       val (_, json) = parseResult(conn)
@@ -249,53 +251,52 @@ object PortalManager {
   })
 
   // Returns: 0 success, 1 error (retry), 2 fatal, and code
-  private def loginCore(conn: URL => URLConnection): (Int, Int) = {
+  private def loginCore(connector: URL => URLConnection): (Int, Int) = {
     Log.d(TAG, "Logging in...")
     try {
-      val chapPassword = autoDisconnect(
-        conn(new URL(HTTP, DOMAIN, "/portal_io/getchallenge")).asInstanceOf[HttpURLConnection]) { conn =>
-        setup(conn)
-        val (code, json) = parseResult(conn)
-        if (code != 0) return null
-        val challenge = (json \ "challenge").asInstanceOf[JString].values
-        val passphrase = new Array[Byte](17)
-        passphrase(0) = Random.nextInt.toByte
-        val passphraseRaw = new mutable.ArrayBuffer[Byte]
-        passphraseRaw += passphrase(0)
-        passphraseRaw ++= password.getBytes
-        passphraseRaw ++= challenge.sliding(2, 2).map(Integer.parseInt(_, 16).toByte)
-        val digest = MessageDigest.getInstance("MD5")
-        digest.update(passphraseRaw.toArray)
-        digest.digest(passphrase, 1, 16)
-        (AUTH_BASE + "&challenge=%s").format(username, passphrase.map("%02X".format(_)).mkString, challenge)
-      }
+      var conn = connector(new URL(HTTP, DOMAIN, "/portal_io/getchallenge")).asInstanceOf[HttpURLConnection]
+      setup(conn)
+      val (code, json) = parseResult(conn)
+      if (code != 0) return null
+      val challenge = (json \ "challenge").asInstanceOf[JString].values
+      val passphrase = new Array[Byte](17)
+      passphrase(0) = Random.nextInt.toByte
+      val passphraseRaw = new mutable.ArrayBuffer[Byte]
+      passphraseRaw += passphrase(0)
+      passphraseRaw ++= password.getBytes
+      passphraseRaw ++= challenge.sliding(2, 2).map(Integer.parseInt(_, 16).toByte)
+      val digest = MessageDigest.getInstance("MD5")
+      digest.update(passphraseRaw.toArray)
+      digest.digest(passphrase, 1, 16)
+      val chapPassword = (AUTH_BASE + "&challenge=%s")
+        .format(username, passphrase.map("%02X".format(_)).mkString, challenge)
       if (chapPassword == null) return (1, 0)
-      autoDisconnect(conn(new URL(HTTP, DOMAIN, "/portal_io/login")).asInstanceOf[HttpURLConnection]) { conn =>
-        setup(conn, chapPassword)
-        conn.getResponseCode match {
-          case 200 =>
-            val (result, obj) = parseResult(conn, true)
-            result match {
-              case 3 => // need manual actions
-                if ((obj \ "reply_msg").toString.startsWith("E011 ")) BalanceManager.cancelNotification() // no more balance
-                (2, result)
-              case 8 =>
-                (2, result)
-              case 1 | 6 =>
-                (0, result)
-              case _ =>
-                (1, result)
-            }
-          case 502 =>
-            app.showToast("无可用服务器资源!")
-            (1, 0)
-          case 503 =>
-            app.showToast("请求太频繁,请稍后再试!")
-            (1, 0)
-          case code =>
-            Log.w(TAG, "Unknown response code: " + code)
-            (2, 0)
-        }
+      conn = connector(new URL(HTTP, DOMAIN, "/portal_io/login")).asInstanceOf[HttpURLConnection]
+      setup(conn, chapPassword)
+      conn.getResponseCode match {
+        case 200 =>
+          val (result, obj) = parseResult(conn, true)
+          result match {
+            case 3 => // need manual actions
+              if ((obj \ "reply_msg").toString.startsWith("E011 ")) // no more balance
+                BalanceManager.cancelNotification()
+              (2, result)
+            case 8 =>
+              (2, result)
+            case 1 | 6 =>
+              (0, result)
+            case _ =>
+              (1, result)
+          }
+        case 502 =>
+          app.showToast("无可用服务器资源!")
+          (1, 0)
+        case 503 =>
+          app.showToast("请求太频繁,请稍后再试!")
+          (1, 0)
+        case i =>
+          Log.w(TAG, "Unknown response code: " + i)
+          (2, 0)
       }
     } catch {
       case e: InvalidResponseException =>
@@ -351,16 +352,15 @@ object PortalManager {
   def openPortalConnection[T](file: String, explicit: Boolean = true)
                              (handler: (HttpURLConnection, AnyRef) => Option[T]) = try {
     val url = new URL(HTTP, DOMAIN, file)
-    var n: AnyRef = null
-    autoDisconnect((if (NetworkMonitor.instance != null && app.boundConnectionsAvailable > 1) {
+    val (n: AnyRef, conn) = if (NetworkMonitor.instance != null && app.boundConnectionsAvailable > 1) {
       val network = NetworkMonitor.instance.listener.preferredNetwork
       if (network == null) throw new NetworkUnavailableException
-      n = network
-      network.openConnection(url)
+      (network, network.openConnection(url))
     } else {
       NetworkMonitor.preferNetworkLegacy()
-      url.openConnection
-    }).asInstanceOf[HttpURLConnection])(handler(_, n))
+      (null, url.openConnection)
+    }
+    handler(conn.asInstanceOf[HttpURLConnection], n)
   } catch {
     case e: NetworkUnavailableException =>
       if (explicit) app.showToast(app.getString(R.string.error_network_unavailable))
