6da163c680b0204494b3a91170d9d1b3003eb73a
Mygod <contact-git@mygod.be>
2016-10-24 16:13:38 +0800

    Add support for shortcuts in Android 7.1

--- src/main/scala/tk/mygod/portal/helper/nju/LogInOutShortcut.scala
+++ src/main/scala/tk/mygod/portal/helper/nju/LogInOutShortcut.scala
@@ -6,18 +6,18 @@ import android.os.Bundle
 import be.mygod.app.ActivityPlus
 
 /**
-  * @author mygod
+  * @author Mygod
   */
-final class ShortcutActivity extends ActivityPlus {
+final class LogInOutShortcut extends ActivityPlus {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     getIntent.getAction match {
       case Intent.ACTION_CREATE_SHORTCUT => setResult(Activity.RESULT_OK, new Intent()
-        .putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent[ShortcutActivity])
+        .putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent[LogInOutShortcut])
         .putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.login_logout))
         .putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext(this, R.drawable.logo)))
       case _ => ThrowableFuture(if (NetworkMonitor.loginStatus == 0) PortalManager.login else PortalManager.logout)
     }
-    finish
+    finish()
   }
 }
